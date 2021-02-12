8c3b6ef5b1e1595cfcc1eafad6f4ef7d0db69f48
leo0807 <zhangjunxu3@gmail.com>
2021-02-12 22:53:59 +0800

    porxy and server

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -7,7 +7,8 @@ import { ApolloProvider } from '@apollo/react-hooks';
 import { setContext } from 'apollo-link-context';
 
 const httpLink = createHttpLink({
-  uri: 'http://localhost:5000'
+  // uri: 'http://localhost:5000'
+  uri: 'https://desolate-oasis-66707.herokuapp.com/'
 });
 
 const authLink = setContext(() => {
